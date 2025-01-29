8ff5a87f8830cae45b620bf97cc620fd0af6cb78
sammymureithi20 <sammymureithi20.com>
2025-01-29 13:20:13 +0300

    deleting unnecessary files

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 
-const postRoute = require("./routes/post");
 const droneRoute = require('./routes/drone');
 const medicationRoute = require('./routes/medication');
 const fleetRoutes = require('./routes/fleets');
@@ -10,7 +9,6 @@ const fleetRoutes = require('./routes/fleets');
 app.use(express.json());
 
 // Use the routes
-app.use("/post", postRoute);
 app.use("/drone", droneRoute);
 app.use('/medication', medicationRoute);
 app.use('/fleets', fleetRoutes);
